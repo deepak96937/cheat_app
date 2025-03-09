c2af2d7798e72bba5df9080e165021c66bd0f1cc
Your Name <your-email@example.com>
2025-03-09 22:54:55 +0530

    add settingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,37 +7,39 @@ import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import SettingPage from './pages/SettingPage'
 import { userAuthStore } from './store/useAuthStore'
-import {Loader} from "lucide-react"
-import {Toaster} from "react-hot-toast"
+import { Loader } from "lucide-react"
+import { Toaster } from "react-hot-toast"
+import { useThemeStore } from './store/useThemeStore'
 
 const App = () => {
 
   const { authUser, checkAuth, isChekingAuth } = userAuthStore()
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
-  
-  if(isChekingAuth && !authUser)
+
+  if (isChekingAuth && !authUser)
+    return (
+      <div className=' flex items-center justify-center h-screen'>
+        <Loader className=" size-10 animate-spin" />
+      </div>
+    )
+
   return (
-    <div className=' flex items-center justify-center h-screen'>
-      <Loader className=" size-10 animate-spin"/>
-    </div>
-  )
-  
-  return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
-        <Route path='/' element={ authUser ? <HomePage />: <Navigate to="login"/>} />
-        <Route path='/signup' element={ !authUser ? <SignUpPage />: <Navigate to="/"/>} />
-        <Route path='/login' element={ !authUser ? <LoginPage /> : <Navigate to="/"/>} />
-        <Route path='/settings' element={ <SettingPage />} />
-        <Route path='/profile' element={authUser ? <ProfilePage />: <Navigate to="login"/>} />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="login" />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/settings' element={<SettingPage />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="login" />} />
       </Routes>
 
-      <Toaster/>
+      <Toaster />
     </div>
   )
 }
