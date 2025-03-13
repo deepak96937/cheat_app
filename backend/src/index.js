063c48435e1125eb405a266ecd251b09dfe8bf68
Your Name <your-email@example.com>
2025-03-13 14:55:03 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,12 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
 
 app.use(
   cors({
@@ -22,6 +25,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT http://localhost:${PORT}`);
